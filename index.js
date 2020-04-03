347a270037c215f7afb6b5caf8f4f17c66c0132c
Ernesto Celis <ernesto@celisdelafuente.net>
2020-04-03 13:20:58 -0600

    Add redis backend

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const BlockChain = require("./blockchain");
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new BlockChain();
+const pubsub = new PubSub({ blockchain });
 
-app.get("/api/blocks", (req, res) => {
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
+app.use(bodyParser.json());
+
+const api = "/api";
+
+app.get(`${api}/blocks`, (req, res) => {
     res.json(blockchain.chain);
+});
+
+app.post(`${api}/mine`, (req, res) => {
+    const { data } = req.body;
+    blockchain.addBlock({ data });
+    res.redirect(`${api}/blocks`);
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`API running in http://localhost:${port}`);
+    console.log(`API running in http://localhost:${port}${api}`);
 });
