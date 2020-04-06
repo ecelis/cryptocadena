8aa557c60e39c243e6db845906bbd3507fd6b91c
Ernesto Celis <ernesto@celisdelafuente.net>
2020-04-06 17:22:56 -0500

    Add REST API and block chain broadcasting

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const BlockChain = require("./blockchain");
 const PubSub = require("./pubsub");
@@ -7,7 +8,8 @@ const app = express();
 const blockchain = new BlockChain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 app.use(bodyParser.json());
 
@@ -20,11 +22,31 @@ app.get(`${api}/blocks`, (req, res) => {
 app.post(`${api}/mine`, (req, res) => {
     const { data } = req.body;
     blockchain.addBlock({ data });
+    pubsub.broadcastChain();
     res.redirect(`${api}/blocks`);
 });
 
-const port = process.env.PORT || 3000;
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+        (error, response, body) => {
+            if(!error && response.statusCode === 200) {
+                const rootChain = JSON.parse(body);
+                console.log('replace chain on a sync with ', rootChain);
+                blockchain.replaceChain(rootChain);
+            }
+        }
+    );
+};
+
+let PEER_PORT;
+if (process.env.GENERATE_PEER_PORT ==='true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+const port = PEER_PORT || DEFAULT_PORT;
 
 app.listen(port, () => {
     console.log(`API running in http://localhost:${port}${api}`);
+    if (port !== DEFAULT_PORT) {
+        syncChains();
+    }
 });
